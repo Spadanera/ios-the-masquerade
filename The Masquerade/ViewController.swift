f3d64ac383759bad9e95b2e1bc7e4e61b15f0d46
Nicola Zirilli <nicola.zirilli@fanplayr.com>
2019-11-27 18:58:00 +0100

    inizialization

--- The Masquerade/ViewController.swift
+++ The Masquerade/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14F89 Safari/602.1"
+        view = webView
+    }
 
     override func viewDidLoad() {
+        let url = URL(string: "http://themasquerade-env.wmujzqm75g.us-east-1.elasticbeanstalk.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
