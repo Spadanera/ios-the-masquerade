87cd123fb13d5d91afeec51b1a75069d37551ce1
Nicola Zirilli <nicola.zirilli@fanplayr.com>
2019-11-27 14:48:05 +0100

    Initial Commit

---
+++ The Masquerade/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Masquerade
+//
+//  Created by Nicola Zirilli on 27/11/2019.
+//  Copyright © 2019 Nicola Zirilli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
